1b1bc9fc03961c3ac90e5577b76f30110952312e
electronicdreamplant <neillawrencephotography@gmail.com>
2022-09-16 11:08:05 +0100

    Update medium-on-website.js

--- JS/medium-on-website.js
+++ JS/medium-on-website.js
@@ -96,8 +96,8 @@ const getCustomPost = (postData) => {
 
       <div id="post-content">
         ${trimContent(postData.postDescription)}...
-        <p id="post-link">
-          <a href="${postData.postLink}"> Continue reading... </a>
+        <p>
+          <a href="${postData.postLink}" class="btn btn-primary"> Continue reading... </a>
         </p>
       </div>
     </div>
